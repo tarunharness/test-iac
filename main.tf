ef98f429999922b85ff3dc967b5744edb354173d
tarunharness <tarun.israni@harness.io>
2026-05-08 16:12:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "harness_platform_workspace" "tp4" {
   repository_path         = ""
   cost_estimation_enabled = true
   provider_connector      = "tarun_vault"
-  repository_connector    = "tarunGitConnector"
+  repository_connector    = "gitamoghmodule"
 
   terraform_variable {
     key        = "instance_name"
