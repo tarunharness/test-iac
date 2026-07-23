c1fdf1f8c8be8b006a9b30fef3fbf7e3565bed70
tarunharness <tarun.israni@harness.io>
2026-07-23 15:46:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,10 +25,10 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+    #aws = {
+    #  source  = "hashicorp/aws"
+    #  version = "~> 5.0"
+    #}
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
