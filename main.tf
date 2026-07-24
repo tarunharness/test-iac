d949ae8af0398fad2452dde7b7fdb8fbdd8c42c4
tarunharness <tarun.israni@harness.io>
2026-07-24 17:57:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 ##############################################################
 # Terraform configuration generating 10,000,000 resources total:
-#   - 500 S3 buckets
-#   - 250 EC2 instances
-#   - 250 SQS queues
+#   - 1000 S3 buckets
+#   - 500 EC2 instances
+#   - 500 SQS queues
 #
 # WARNING: This is NOT deployable against a real AWS account.
 #   - S3: default account limit is ~100 buckets (soft-raisable
@@ -53,19 +53,19 @@ variable "aws_region" {
 variable "s3_bucket_count" {
   description = "Number of S3 buckets to create (NOTE: AWS hard-limits accounts to ~100 buckets by default; this value is for HCL/code-gen scale-testing only, not a realistic deploy target)"
   type        = number
-  default     = 500
+  default     = 1000
 }
 
 variable "ec2_instance_count" {
   description = "Number of EC2 instances to create (NOTE: will hit EC2 vCPU quota almost immediately; not realistically deployable at this scale)"
   type        = number
-  default     = 250
+  default     = 500
 }
 
 variable "sqs_queue_count" {
   description = "Number of SQS queues to create"
   type        = number
-  default     = 250
+  default     = 500
 }
 
 variable "ec2_ami" {
@@ -113,7 +113,7 @@ resource "random_id" "bucket_suffix" {
 }
 
 ##############################################################
-# S3 Buckets  (500 resources)
+# S3 Buckets  (1000 resources)
 ##############################################################
 
 resource "aws_s3_bucket" "bulk" {
@@ -130,7 +130,7 @@ resource "aws_s3_bucket" "bulk" {
 }
 
 ##############################################################
-# EC2 Instances 250 resources)
+# EC2 Instances 500 resources)
 ##############################################################
 
 resource "aws_instance" "bulk" {
@@ -148,7 +148,7 @@ resource "aws_instance" "bulk" {
 }
 
 ##############################################################
-# SQS Queues (250 resources)
+# SQS Queues (500 resources)
 ##############################################################
 
 resource "aws_sqs_queue" "bulk" {
