7de1c308361035d0082d7f633a90088ad148245c
tarunharness <tarun.israni@harness.io>
2026-07-27 15:40:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,10 +28,22 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "sqs_queue_count" {
-  description = "Number of SQS queues to create"
+variable "ec2_instance_count" {
+  description = "Number of EC2 instances to create (NOTE: will hit EC2 vCPU quota almost immediately; not realistically deployable at this scale)"
   type        = number
   default     = 2000
+}
+
+variable "ec2_ami" {
+  description = "AMI ID to use for EC2 instances (defaults to latest Amazon Linux 2023 if not overridden)"
+  type        = string
+  default     = ""
+}
+
+variable "ec2_instance_type" {
+  description = "Instance type for generated EC2 instances"
+  type        = string
+  default     = "t3.micro"
 }
 
 variable "name_prefix" {
@@ -41,21 +53,44 @@ variable "name_prefix" {
 }
 
 ##############################################################
-# SQS Queues (1000 resources)
+# Data sources
 ##############################################################
 
-resource "aws_sqs_queue" "bulk" {
-  count = var.sqs_queue_count
+# Used only if var.ec2_ami is left empty
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
-  name = "${var.name_prefix}-sqs-${count.index}"
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+##############################################################
+# EC2 Instances 1000 resources)
+##############################################################
+
+resource "aws_instance" "bulk" {
+  count = var.ec2_instance_count
+
+  ami           = var.ec2_ami != "" ? var.ec2_ami : data.aws_ami.amazon_linux.id
+  instance_type = var.ec2_instance_type
 
   tags = {
-    Name      = "${var.name_prefix}-sqs-${count.index}"
+    Name      = "${var.name_prefix}-ec2-${count.index}"
     Index     = count.index
-    Type      = "sqs-queue"
+    Type      = "ec2-instance"
     ManagedBy = "terraform"
   }
 }
+
+
 
 ##############################################################
 # Outputs
@@ -63,10 +98,10 @@ resource "aws_sqs_queue" "bulk" {
 
 output "total_resource_count" {
   description = "Total number of primary resources created by this configuration"
-  value       = var.sqs_queue_count
+  value       = var.ec2_instance_count
 }
 
-output "sqs_queue_urls" {
-  description = "URLs of created SQS queues"
-  value       = aws_sqs_queue.bulk[*].id
+output "ec2_instance_ids" {
+  description = "IDs of created EC2 instances"
+  value       = aws_instance.bulk[*].id
 }
